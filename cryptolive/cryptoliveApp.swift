59eb169f34fc45bbf7fee712e4f9bf8792a24fa3
ouwargui <guikemon2@gmail.com>
2022-11-21 16:32:55 -0300

    chore: first commit

---
+++ cryptolive/cryptoliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoliveApp.swift
+//  cryptolive
+//
+//  Created by Guilherme Santos on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct cryptoliveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
